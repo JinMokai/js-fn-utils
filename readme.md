4c2e0337b223459c54e59914bf2b8e6206c8671f
jinkai <ch_kaijack@outlook.com>
2024-10-26 19:16:53 +0800

    chore: init

--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
 # @jinmokai/utils
 
 this is some javascrpt function utils.
+
+## install
+
+```bash
+pnpm i @jinmokai/utils
+```
+
+## usage
+
+you can use browser or nodejs,it is support ESM and CJS.
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,6 @@
 import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
+import terser from '@rollup/plugin-terser'
 
 export default [
     // .d.ts file build
@@ -17,6 +18,7 @@ export default [
     output: {
       dir: "dist/esm",
       format: "es",
+      entryFileNames: '[name].mjs',
       preserveModules: true,
       sourcemap: true,
     },
@@ -33,10 +35,43 @@ export default [
         format: "cjs",
         preserveModules: true,
         sourcemap: true,
+        entryFileNames: '[name].cjs',
     },
     plugins: [typescript({
         outDir: 'dist/cjs',
         removeComments: true
     })],
+  },
+  // cdn build
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        format: 'iife',
+        preserveModules: false,
+        file: 'cdn/index.js',
+        name: 'index',
+        strict: true
+      },
+      {
+        format: 'iife',
+        preserveModules: false,
+        file: 'cdn/index.min.js',
+        name: 'index',
+        strict: true,
+        plugins: [terser()]
+      },
+      {
+        format: 'es',
+        preserveModules: false,
+        name: 'index',
+        file: 'cdn/index.esm.js',
+        strict: true
+      }
+    ],
+    plugins: [typescript({
+        outDir: 'cdn',
+        removeComments: true
+    })]
   }
 ];
